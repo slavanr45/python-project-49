0465183b7c5976bbf5c19fbaeda6fd6d4adf22a8
slavanr45 <slava.r45@gmail.com>
2023-05-30 00:03:53 +0700

    fix brain-progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,7 @@ def game():
     step = rd.randint(1, 5)
     length = rd.randint(5, 15)
     lst = list(range(start, start + step * length, step))
-    i = rd.randint(0, length-1)
+    i = rd.randint(0, length - 1)
     result = lst[i]
     lst[i] = '..'
     print('What number is missing in the progression?')
